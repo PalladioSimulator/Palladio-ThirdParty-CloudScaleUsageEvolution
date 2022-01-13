ca108e2504ebf5755bffe5e156af5d54b9315781
Nathan <nathan@hagel.dev>
2022-01-13 21:57:57 +0100

    Remove only generated classes/code and add src-gen folder

--- bundles/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/WorkParameterEvolutionItemProvider.java
+++ bundles/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/WorkParameterEvolutionItemProvider.java
@@ -27,33 +27,14 @@ import org.scaledl.usageevolution.UsageevolutionPackage;
  * <!-- begin-user-doc --> <!-- end-user-doc -->
  * @generated
  */
-public class WorkParameterEvolutionItemProvider extends ItemProviderAdapter
-		implements IEditingDomainItemProvider, IStructuredItemContentProvider,
-		ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+public class WorkParameterEvolutionItemProvider extends WorkParameterEvolutionItemProviderGen {
 	/**
 	 * This constructs an instance from a factory and a notifier.
 	 * <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
-	 * @generated
 	 */
 	public WorkParameterEvolutionItemProvider(AdapterFactory adapterFactory) {
 		super(adapterFactory);
-	}
-
-	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * @generated
-	 */
-	public List<IItemPropertyDescriptor> getPropertyDescriptorsGen(Object object) {
-		if (itemPropertyDescriptors == null) {
-			super.getPropertyDescriptors(object);
-
-			addEvolutionPropertyDescriptor(object);
-			addVariableCharacterisationPropertyDescriptor(object);
-		}
-		return itemPropertyDescriptors;
 	}
 
 	/**
@@ -68,26 +49,6 @@ public class WorkParameterEvolutionItemProvider extends ItemProviderAdapter
 		itemPropertyDescriptors = null; // Force rebuild
 		this.getPropertyDescriptorsGen(object);
 		return this.itemPropertyDescriptors;
-	}
-
-	/**
-	 * This adds a property descriptor for the Evolution feature.
-	 * <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * @generated
-	 */
-	protected void addEvolutionPropertyDescriptor(Object object) {
-		itemPropertyDescriptors
-				.add(createItemPropertyDescriptor(
-						((ComposeableAdapterFactory) adapterFactory)
-								.getRootAdapterFactory(),
-						getResourceLocator(),
-						getString("_UI_WorkParameterEvolution_evolution_feature"),
-						getString("_UI_PropertyDescriptor_description",
-								"_UI_WorkParameterEvolution_evolution_feature",
-								"_UI_WorkParameterEvolution_type"),
-						UsageevolutionPackage.Literals.WORK_PARAMETER_EVOLUTION__EVOLUTION,
-						true, false, true, null, null, null));
 	}
 
 	class VariableCharacterisationPropertyDescriptor extends
@@ -175,65 +136,6 @@ public class WorkParameterEvolutionItemProvider extends ItemProviderAdapter
 						UsageevolutionPackage.Literals.WORK_PARAMETER_EVOLUTION__VARIABLE_CHARACTERISATION,
 						true, false, true, null, null, null));
 
-	}
-
-	/**
-	 * This returns WorkParameterEvolution.gif.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public Object getImage(Object object) {
-		return overlayImage(
-				object,
-				getResourceLocator().getImage(
-						"full/obj16/WorkParameterEvolution"));
-	}
-
-	/**
-	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
-	 * -->
-	 *
-	 * @generated
-	 */
-	@Override
-	public String getText(Object object) {
-		return getString("_UI_WorkParameterEvolution_type");
-	}
-
-	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void notifyChanged(Notification notification) {
-		updateChildren(notification);
-		super.notifyChanged(notification);
-	}
-
-	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected void collectNewChildDescriptors(
-			Collection<Object> newChildDescriptors, Object object) {
-		super.collectNewChildDescriptors(newChildDescriptors, object);
-	}
-
-	/**
-	 * Return the resource locator for this item provider's resources.
-	 * <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public ResourceLocator getResourceLocator() {
-		return UsageevolutionEditPlugin.INSTANCE;
 	}
 
 }
