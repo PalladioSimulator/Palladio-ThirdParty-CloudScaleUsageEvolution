dd2fbba1c3e24358bf89ed3a9c6223e6fafafa4d
Nathan <nathan@hagel.dev>
2022-01-19 16:48:12 +0100

    Not working version, path problem fixed.

--- bundles/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/UsageEvolutionItemProvider.java
+++ bundles/org.scaledl.usageevolution.edit/src/org/scaledl/usageevolution/provider/UsageEvolutionItemProvider.java
@@ -11,31 +11,24 @@ import org.eclipse.emf.common.notify.Notification;
 
 import org.eclipse.emf.common.util.ResourceLocator;
 
-import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
-import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
-import org.eclipse.emf.edit.provider.IItemLabelProvider;
-import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
-import org.eclipse.emf.edit.provider.IItemPropertySource;
-import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
-import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+import org.eclipse.emf.ecore.EStructuralFeature;
 
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import org.palladiosimulator.pcm.core.entity.provider.EntityItemProvider;
+
+import org.scaledl.usageevolution.UsageEvolution;
+import org.scaledl.usageevolution.UsageevolutionFactory;
 import org.scaledl.usageevolution.UsageevolutionPackage;
 
 /**
- * This is the item provider adapter for a {@link org.scaledl.usageevolution.WorkParameterEvolution} object.
+ * This is the item provider adapter for a {@link org.scaledl.usageevolution.UsageEvolution} object.
  * <!-- begin-user-doc -->
  * <!-- end-user-doc -->
  * @generated
  */
-public class WorkParameterEvolutionItemProvider 
-	extends ItemProviderAdapter
-	implements
-		IEditingDomainItemProvider,
-		IStructuredItemContentProvider,
-		ITreeItemContentProvider,
-		IItemLabelProvider,
-		IItemPropertySource
+public class UsageEvolutionItemProvider extends EntityItemProvider
 {
 	/**
 	 * This constructs an instance from a factory and a notifier.
@@ -43,7 +36,7 @@ public class WorkParameterEvolutionItemProvider
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public WorkParameterEvolutionItemProvider(AdapterFactory adapterFactory)
+	public UsageEvolutionItemProvider(AdapterFactory adapterFactory)
 	{
 		super(adapterFactory);
 	}
@@ -61,60 +54,45 @@ public class WorkParameterEvolutionItemProvider
 		{
 			super.getPropertyDescriptors(object);
 
-			addEvolutionPropertyDescriptor(object);
-			addVariableCharacterisationPropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
 
 	/**
-	 * This adds a property descriptor for the Evolution feature.
+	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
+	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	protected void addEvolutionPropertyDescriptor(Object object)
+	@Override
+	public Collection<? extends EStructuralFeature> getChildrenFeatures(Object object)
 	{
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_WorkParameterEvolution_evolution_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_WorkParameterEvolution_evolution_feature", "_UI_WorkParameterEvolution_type"),
-				 UsageevolutionPackage.Literals.WORK_PARAMETER_EVOLUTION__EVOLUTION,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+		if (childrenFeatures == null)
+		{
+			super.getChildrenFeatures(object);
+			childrenFeatures.add(UsageevolutionPackage.Literals.USAGE_EVOLUTION__USAGES);
+		}
+		return childrenFeatures;
 	}
 
 	/**
-	 * This adds a property descriptor for the Variable Characterisation feature.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	protected void addVariableCharacterisationPropertyDescriptor(Object object)
+	@Override
+	protected EStructuralFeature getChildFeature(Object object, Object child)
 	{
-		itemPropertyDescriptors.add
-			(createItemPropertyDescriptor
-				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
-				 getResourceLocator(),
-				 getString("_UI_WorkParameterEvolution_variableCharacterisation_feature"),
-				 getString("_UI_PropertyDescriptor_description", "_UI_WorkParameterEvolution_variableCharacterisation_feature", "_UI_WorkParameterEvolution_type"),
-				 UsageevolutionPackage.Literals.WORK_PARAMETER_EVOLUTION__VARIABLE_CHARACTERISATION,
-				 true,
-				 false,
-				 true,
-				 null,
-				 null,
-				 null));
+		// Check the type of the specified child object and return the proper feature to use for
+		// adding (see {@link AddCommand}) it as a child.
+
+		return super.getChildFeature(object, child);
 	}
 
 	/**
-	 * This returns WorkParameterEvolution.gif.
+	 * This returns UsageEvolution.gif.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
@@ -122,7 +100,7 @@ public class WorkParameterEvolutionItemProvider
 	@Override
 	public Object getImage(Object object)
 	{
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/WorkParameterEvolution"));
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/UsageEvolution"));
 	}
 
 	/**
@@ -134,7 +112,10 @@ public class WorkParameterEvolutionItemProvider
 	@Override
 	public String getText(Object object)
 	{
-		return getString("_UI_WorkParameterEvolution_type");
+		String label = ((UsageEvolution)object).getEntityName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_UsageEvolution_type") :
+			getString("_UI_UsageEvolution_type") + " " + label;
 	}
 
 
@@ -149,6 +130,13 @@ public class WorkParameterEvolutionItemProvider
 	public void notifyChanged(Notification notification)
 	{
 		updateChildren(notification);
+
+		switch (notification.getFeatureID(UsageEvolution.class))
+		{
+			case UsageevolutionPackage.USAGE_EVOLUTION__USAGES:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), true, false));
+				return;
+		}
 		super.notifyChanged(notification);
 	}
 
@@ -163,6 +151,11 @@ public class WorkParameterEvolutionItemProvider
 	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object)
 	{
 		super.collectNewChildDescriptors(newChildDescriptors, object);
+
+		newChildDescriptors.add
+			(createChildParameter
+				(UsageevolutionPackage.Literals.USAGE_EVOLUTION__USAGES,
+				 UsageevolutionFactory.eINSTANCE.createUsage()));
 	}
 
 	/**
